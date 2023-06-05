4093514c4d266270a7c12bbdf4d7800800529285
unknown <umerarif01@gmail.com>
2023-06-05 20:13:39 +0500

    first commit

---
+++ predict_speaker.py
@@ -0,0 +1,65 @@
+# pip install joblib librosa numpy sounddevice soundfile
+import joblib
+import librosa
+import numpy as np
+import sounddevice as sd
+import soundfile as sf
+import os
+import random
+
+sample_rate = 44100 
+duration = 5 
+
+model = joblib.load('SVC_model_34ssamples.joblib')
+
+def extract_features(file_path):
+    audio, sample_rate = librosa.load(file_path, res_type='kaiser_fast')
+    mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=35)
+    mfccs_scaled = np.mean(mfccs.T, axis=0)
+    return mfccs_scaled
+
+data_dir = 'test'
+expected= []
+predicted = []
+
+audios = []
+
+for file_name in os.listdir(data_dir):
+    audios.append(file_name)
+
+# print(audios)
+
+count = {}
+
+def voicesamples():
+    for audio_file in audios:
+        file_path = os.path.join(data_dir, audio_file)
+        if file_path.endswith('.flac') or file_path.endswith('.wav'):
+            extracted_features = extract_features(file_path)
+            extracted_features = extracted_features.reshape(1, -1)
+            prediction = model.predict(extracted_features)
+            expected.append(audio_file)
+            predicted.append(prediction[0])
+            if prediction[0] not in count:
+                count[prediction[0]] = 1
+            else:
+                count[prediction[0]] += 1
+
+def record():
+    print("Recording...")
+    audio = sd.rec(int(sample_rate * duration), samplerate=sample_rate, channels=1)
+    sd.wait()
+    file_path = "predict.wav"
+    sf.write(file_path, audio, sample_rate)
+    new_features = extract_features("predict.wav")
+    print(new_features)
+    new_features = new_features.reshape(1, -1)
+    new_prediction = model.predict(new_features)
+    print("Prediction:", new_prediction)
+
+print()
+voicesamples()
+print("Files\t\tPredicted")
+for i in range(len(expected)):
+    print(expected[i],"\t",predicted[i])
+print("count: ", count)
